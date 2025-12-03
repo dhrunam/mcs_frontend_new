20c12d3b269ee58ce3bcc7855208d2543e90c5af
dhrunam <djb.sibin@gmail.com>
2025-12-03 09:55:47 +0530

    change the environment

--- frontend/src/environment/environment.ts
+++ frontend/src/environment/environment.ts
@@ -1,7 +1,7 @@
-export const URL = 'http://10.182.144.249:8001/api'
+// export const URL = 'http://10.182.144.249:8001/api'
 // export const URL = "http://10.182.144.12:8001/api"
 // export const URL = "http://10.182.144.110:8000/api"
-// export const URL = "http://localhost:8000/api"
+export const URL = "http://localhost:8000/api"
 
 // export const environment = {
 //     production: false,
